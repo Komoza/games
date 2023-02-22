3832ee41325c9fdeed5d54719b19c6abab9230b4
Maxim <MaximKomoza@yandex.ru>
2023-02-22 23:11:06 +1000

    check busy cell && event keydonw

--- script/2048.js
+++ script/2048.js
@@ -1,10 +1,13 @@
 const cells = document.querySelector('.game2048__cells');
 const restart = document.querySelector('.game2048__restart')
+let cellStatus = [];
 
 createRandomUnit();
 
+
 restart.onclick = (event) => {
-    cleanBorad()
+    cleanBorad();
+    cellStatus = [];
     createRandomUnit();
 }
 
@@ -13,14 +16,19 @@ function createRandomUnit() {
     unit.className = 'game2048__unit';
     unit.innerHTML = Math.random() < 0.8 ? '2' : '4';
 
-    let posX = Math.floor(Math.random() * 4 + 1);
-    let posY = Math.floor(Math.random() * 4 + 1);
+    const posX = Math.floor(Math.random() * 4 + 1);
+    const posY = Math.floor(Math.random() * 4 + 1);
 
-    unit.style.setProperty('--posX', posX);
-    unit.style.setProperty('--posY', posY);
-    unit.style.setProperty('background', getColor(unit));
-
-    cells.appendChild(unit);
+    if (!isBusy(posX, posY)){
+        unit.style.setProperty('--posX', posX);
+        unit.style.setProperty('--posY', posY);
+        unit.style.setProperty('background', getColor(unit));
+    
+        cells.appendChild(unit);
+        cellStatus.push(`${posX}${posY}`);
+    }else {
+        createRandomUnit();
+    }
 }
 
 function cleanBorad() {
@@ -30,24 +38,28 @@ function cleanBorad() {
     })
 }
 
+function isBusy(x, y) {
+    return cellStatus.includes(`${x}${y}`);
+}
+
 function getColor(cell) {
     switch(cell.innerHTML) {
         case '2':
-            return '#f0cfa1';
+            return 'red';
         case '4':
-            return '#edb05a';
+            return 'orange';
         case '8':
-            return '#edb05a';
+            return 'yellow';
         case '16':
-            return '#edb05a';
+            return 'green';
         case '32':
-            return '#edb05a';
+            return 'blue';
         case '64':
-            return '#edb05a';
+            return 'darkblue';
         case '128':
-            return '#edb05a';
+            return 'purple';
         case '256':
-            return '#edb05a';
+            return 'red';
         case '512':
             return '#edb05a';
         case '1024':
@@ -59,4 +71,19 @@ function getColor(cell) {
             return '#f0cfa1';
     }
 }
+
+document.addEventListener('keydown', function drawLetter(event) {
+    if (event.code == 'ArrowUp') {
+        console.log('up')
+    }
+    if (event.code == 'ArrowDown') {
+        console.log('down')
+    }
+    if (event.code == 'ArrowLeft') {
+        console.log('left')
+    }
+    if (event.code == 'ArrowRight') {
+        console.log('right')
+    }
+})
 
