4b6c13a1cce6d4a672b21c9ef2bae95453da0c70
Maxim <MaximKomoza@yandex.ru>
2023-02-22 20:13:24 +1000

    restart game

--- script/2048.js
+++ script/2048.js
@@ -1,13 +1,27 @@
-const gameBoard = document.querySelector('.game-board');
+const cells = document.querySelector('.game2048__cells');
+const restart = document.querySelector('.game2048__restart')
+
 createRandomUnit();
+
+restart.onclick = (event) => {
+    cleanBorad()
+    createRandomUnit();
+}
 
 function createRandomUnit() {
     const unit = document.createElement('div');
-    unit.className = 'unit';
+    unit.className = 'game2048__unit';
     unit.innerHTML = Math.random() < 0.7 ? '2' : '4';
     unit.style.setProperty('--posX', Math.floor(Math.random() * 4 + 1));
     unit.style.setProperty('--posY', Math.floor(Math.random() * 4 + 1));
-    gameBoard.append(unit);
+    cells.appendChild(unit);
+}
+
+function cleanBorad() {
+    const units = document.querySelectorAll('.game2048__unit');
+    units.forEach((item) => {
+        cells.removeChild(item);
+    })
 }
 
 
